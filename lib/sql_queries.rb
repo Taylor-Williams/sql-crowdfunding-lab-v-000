70ecf9ceefbaa0981623dcb97731adc6d1fade49
Taylor Williams <taylor@thewilliams.net>
2018-07-09 21:25:58 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 # if the total of the pledges is greater than the project goal then display the title and sum of pledges - goal
 "SELECT projects.title, projects.funding_goal, SUM(pledges.amount) FROM projects
 INNER JOIN pledges ON projects.id = pledges.project_id
-GROUP BY projects.title HAVING SUM(pledges.amount) > projects.funding_goal"
+GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
